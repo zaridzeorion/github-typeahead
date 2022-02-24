1962aae4619d4a4332ff6650cff7dc8794dc7b14
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-02-24 20:38:47 +0400

    User searching added
    Github users now are searchable by input field

--- src/App.js
+++ src/App.js
@@ -2,19 +2,32 @@ import React, { useEffect, useState } from "react";
 import { GET } from "./fetch.js";
 
 function App() {
+  const [input, setInput] = useState("");
   const [users, setUsers] = useState("");
-  const BASE_URL = `https://api.github.com/users`;
+  const BASE_URL = `https://api.github.com/search/users?q=${input}+in:login`;
 
   useEffect(() => {
-    GET(BASE_URL).then((res) => setUsers(res));
-  }, []);
+    GET(BASE_URL).then((res) => setUsers(res.items));
+  }, [BASE_URL, input]);
 
   return (
-    <ul>
-      {users
-        ? users.map((user) => <li key={user.id}>{user.login}</li>)
-        : "Loading..."}
-    </ul>
+    <div>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          setInput(e.target.value);
+        }}
+      />
+
+      <ul>
+        {users
+          ? users.map((user) => <li key={user.id}>{user.login}</li>)
+          : "Loading..."}
+
+        {users.length === 0 && <li>"No users found."</li>}
+      </ul>
+    </div>
   );
 }
 
