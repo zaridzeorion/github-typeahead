022ff41c6e5a350e692af5cebf33ef04668f50dc
Zurab Zaridze <zaridzezurab0@gmail.com>
2022-02-28 21:56:29 +0400

    Added error handling for fetch method

--- src/fetch.js
+++ src/fetch.js
@@ -1,5 +1,10 @@
 export async function GET(url) {
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
+  try {
+    let response = await fetch(url);
+    if (!response.ok) throw new Error(response.statusText);
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
